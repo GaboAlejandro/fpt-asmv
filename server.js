fbe0122bc2864221fed218e2edb4d6ffc7ff1104
Gabriel <lavingabo012@gmail.com>
2019-08-16 14:46:18 -0400

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var bodyParser = require('body-parser');
 
-const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}) );
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/expense-app'));
+app.use( express.static(__dirname + '/client' ) );
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+var listener = server.listen(process.env.PORT || 5000, function(){
+  console.log('Listening on port ' + listener.address().port); //Listening on port 5000
+});
