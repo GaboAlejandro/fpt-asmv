9295d0815bdfbcc89ec67f06f93d1c7d0cb48d19
Gabriel <lavingabo012@gmail.com>
2019-08-16 16:40:19 -0400

    configure package.json

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,20 +1,19 @@
 import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { AppointmentComponent } from './components/appointment/appointment.component';
 import { DancingComponent } from './components/dancing/dancing.component';
 import { DancersComponent } from './components/dancers/dancers.component';
-
+import { Routes, RouterModule } from '@angular/router';
 
 
 
 const appRoutes: Routes = [
 
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent},
   { path: 'appointment', component: AppointmentComponent},
   { path: 'dancing', component: DancingComponent},
   { path: 'dancers', component: DancersComponent},
-  { path: '',  redirectTo: '/login', pathMatch: 'full'},
   { path: '**', redirectTo: '/login', pathMatch: 'full'},
 
 ];
